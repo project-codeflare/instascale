b4d92b47ed1ab4c23a7dd5ba95becf9d9d04a909
Eoin Gallinagh <eoingallinagh@gmail.com>
2024-01-03 17:34:07 +0000

    add: Hypershift Functionality

---
+++ controllers/nodepools.go
@@ -0,0 +1,139 @@
+package controllers
+
+import (
+	"context"
+	"strings"
+
+	cmv1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
+	arbv1 "github.com/project-codeflare/multi-cluster-app-dispatcher/pkg/apis/controller/v1beta1"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+func (r *AppWrapperReconciler) scaleNodePool(ctx context.Context, aw *arbv1.AppWrapper, demandPerInstanceType map[string]int) (ctrl.Result, error) {
+	logger := ctrl.LoggerFrom(ctx)
+	connection, err := r.createOCMConnection()
+	if err != nil {
+		logger.Error(err, "Error creating OCM connection")
+		return ctrl.Result{}, err
+	}
+	defer connection.Close()
+	for userRequestedInstanceType := range demandPerInstanceType {
+		replicas := demandPerInstanceType[userRequestedInstanceType]
+
+		clusterNodePools := connection.ClustersMgmt().V1().Clusters().Cluster(r.ocmClusterID).NodePools()
+
+		response, err := clusterNodePools.List().SendContext(ctx)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		numberOfMachines := 0
+		response.Items().Each(func(nodePool *cmv1.NodePool) bool {
+			if nodePool.AWSNodePool().InstanceType() == userRequestedInstanceType && hasAwLabel(nodePool.Labels(), aw) {
+				numberOfMachines = nodePool.Replicas()
+				return false
+			}
+			return true
+		})
+
+		if numberOfMachines != replicas {
+			m := make(map[string]string)
+			m[aw.Name] = aw.Name
+			logger.Info("The instanceRequired array",
+				"InstanceRequired", userRequestedInstanceType)
+
+			nodePoolID := strings.ReplaceAll(aw.Name+"-"+userRequestedInstanceType, ".", "-")
+
+			createNodePool, err := cmv1.NewNodePool().AWSNodePool(cmv1.NewAWSNodePool().InstanceType(userRequestedInstanceType)).ID(nodePoolID).Replicas(replicas).Labels(m).Build()
+			if err != nil {
+				logger.Error(
+					err, "Error building NodePool",
+					"userRequestedInstanceType", userRequestedInstanceType,
+				)
+			}
+			logger.Info(
+				"Sending NodePool creation request",
+				"instanceType", userRequestedInstanceType,
+				"nodePoolName", createNodePool.ID(),
+			)
+			response, err := clusterNodePools.Add().Body(createNodePool).SendContext(ctx)
+			if err != nil {
+				logger.Error(err, "Error creating NodePool")
+			} else {
+				logger.Info(
+					"Successfully created NodePool",
+					"nodePoolName", createNodePool.ID(),
+					"response", response,
+				)
+			}
+		}
+	}
+	return ctrl.Result{Requeue: false}, nil
+}
+
+func (r *AppWrapperReconciler) deleteNodePool(ctx context.Context, aw *arbv1.AppWrapper) (ctrl.Result, error) {
+	logger := ctrl.LoggerFrom(ctx)
+	connection, err := r.createOCMConnection()
+	if err != nil {
+		logger.Error(err, "Error creating OCM connection")
+		return ctrl.Result{}, err
+	}
+	defer connection.Close()
+
+	nodePoolsConnection := connection.ClustersMgmt().V1().Clusters().Cluster(r.ocmClusterID).NodePools().List()
+
+	nodePoolsListResponse, _ := nodePoolsConnection.Send()
+	nodePoolsList := nodePoolsListResponse.Items()
+	nodePoolsList.Range(func(index int, item *cmv1.NodePool) bool {
+		id, _ := item.GetID()
+		if strings.Contains(id, aw.Name) {
+			targetNodePool, err := connection.ClustersMgmt().V1().Clusters().Cluster(r.ocmClusterID).NodePools().NodePool(id).Delete().SendContext(ctx)
+			if err != nil {
+				logger.Error(
+					err, "Error deleting nodepool",
+					"nodePool", targetNodePool,
+				)
+			} else {
+				logger.Info(
+					"Successfully scaled down target nodepool",
+					"nodePool", targetNodePool,
+				)
+			}
+		}
+		return true
+	})
+	return ctrl.Result{Requeue: false}, nil
+}
+
+func (r *AppWrapperReconciler) checkHypershiftEnabled(ctx context.Context) (bool, error) {
+	logger := ctrl.LoggerFrom(ctx)
+	connection, err := r.createOCMConnection()
+	if err != nil {
+		logger.Error(err, "Error creating OCM connection")
+		return false, err
+	}
+	defer connection.Close()
+
+	clusterResource := connection.ClustersMgmt().V1().Clusters().Cluster(r.ocmClusterID)
+
+	response, err := clusterResource.Get().SendContext(ctx)
+	if err != nil {
+		logger.Error(err, "error fetching cluster details")
+		return false, err
+	}
+
+	body := response.Body()
+	if body == nil {
+		logger.Error(err, "Empty resource body when checking Hypershift enabled status")
+		return false, err
+	}
+
+	hypershiftEnabled := false
+	if body.Hypershift() != nil {
+		hypershiftEnabled = body.Hypershift().Enabled()
+	}
+
+	logger.Info("Checked Hypershift enabled status", "status", hypershiftEnabled)
+	return hypershiftEnabled, nil
+}
---
+++ controllers/ocm_manager.go
@@ -0,0 +1,77 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	ocmsdk "github.com/openshift-online/ocm-sdk-go"
+	cmv1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
+	configv1 "github.com/openshift/api/config/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+func (r *AppWrapperReconciler) createOCMConnection() (*ocmsdk.Connection, error) {
+	logger, err := ocmsdk.NewGoLoggerBuilder().
+		Debug(false).
+		Build()
+	if err != nil {
+		return nil, fmt.Errorf("can't build logger: %v", err)
+	}
+
+	connection, err := ocmsdk.NewConnectionBuilder().
+		Logger(logger).
+		Tokens(r.ocmToken).
+		Build()
+	if err != nil {
+		return nil, fmt.Errorf("can't build connection: %v", err)
+	}
+
+	r.ocmConnection = connection
+	return r.ocmConnection, nil
+}
+
+// getOCMClusterID determines the internal clusterID to be used for OCM API calls
+func (r *AppWrapperReconciler) getOCMClusterID(ctx context.Context) error {
+	logger := ctrl.LoggerFrom(ctx)
+	cv := &configv1.ClusterVersion{}
+	err := r.Get(ctx, types.NamespacedName{Name: "version"}, cv)
+	if err != nil {
+		logger.Error(err, "can't get clusterversion")
+		return err
+	}
+
+	internalClusterID := string(cv.Spec.ClusterID)
+
+	connection, err := r.createOCMConnection()
+	if err != nil {
+		logger.Error(err, "Error creating OCM connection")
+	}
+	defer connection.Close()
+
+	// Get the client for the resource that manages the collection of clusters:
+	collection := connection.ClustersMgmt().V1().Clusters()
+
+	response, err := collection.List().Search(fmt.Sprintf("external_id = '%s'", internalClusterID)).Size(1).Page(1).SendContext(ctx)
+	if err != nil {
+		logger.Error(err, "Error getting cluster id")
+	}
+
+	response.Items().Each(func(cluster *cmv1.Cluster) bool {
+		r.ocmClusterID = cluster.ID()
+		logger.Info(
+			"Cluster Info",
+			"clusterId", cluster.ID(),
+			"clusterName", cluster.Name(),
+			"clusterState", cluster.State(),
+		)
+		return true
+	})
+	return nil
+}
+
+func (r *AppWrapperReconciler) Close() {
+	if r.ocmConnection != nil {
+		r.ocmConnection.Close()
+	}
+}
